23489247c18aa33dee0eacb37aa18843a43458d7
Angel Concepcion <angelconcepcion50@gmail.com>
2022-06-27 11:50:50 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[10];
+        for (int i = 0; i < names.length; i++){
+            System.out.println("Enter Student's name:");
+            String studentName =  scanner.next();
+            names[i] = studentName;
+        }
+        System.out.println("You have entered the following names:");
+        for (String name : names){
+            System.out.println(name);
+        }
     }
 }
